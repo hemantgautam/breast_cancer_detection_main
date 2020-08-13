5b7dc8e92b0931821b17694f7bb59e5704392903
jane <alam.jane61@gmail.com>
2020-08-13 16:05:14 +0530

    ppt

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def downloadPredictedCsv():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
+    app.run(port=config.PORT, debug=config.DEBUG_MODE)
